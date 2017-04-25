8ccc333b78080f584e5c30a4cce3bcd76010c871
Kevin Zúñiga <kevin.zun@gmail.com>
2017-04-25 16:32:04 -0500

    Add Ace editor

---
+++ assets/js/editor.js
@@ -0,0 +1,9 @@
+import ace from "brace"
+import "brace/theme/dawn"
+import "brace/mode/markdown"
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/dawn");
+editor.getSession().setMode("ace/mode/markdown");
+editor.getSession().setUseSoftTabs(true);
+editor.renderer.setShowPrintMargin(false);
