ea45271df2e89d8516b81223ffe17df22004749b
Kevin Zúñiga <kevin.zun@gmail.com>
2017-04-26 16:25:48 -0500

    Add code to load bloc from redis

---
+++ lib/phoenix_bloc/bloc/bloc_controller.ex
@@ -0,0 +1,10 @@
+defmodule PhoenixBloc.Bloc.BlocController do
+  alias Redix
+
+  def get_bloc(bloc_id) do
+    case Redix.command(:redix, ~w(HMGET bloc:#{bloc_id} rev content)) do
+      {:ok, [:nil, _]} -> {:ok, "0", ""}
+      {:ok, [rev, content]} -> {:ok, rev, content}
+    end
+  end
+end
--- lib/phoenix_bloc/web/controllers/page_controller.ex
+++ lib/phoenix_bloc/web/controllers/page_controller.ex
@@ -1,7 +1,11 @@
 defmodule PhoenixBloc.Web.PageController do
+  alias Redix
   use PhoenixBloc.Web, :controller
+  import PhoenixBloc.Bloc.BlocController
 
   def index(conn, _params) do
-    render conn, "index.html"
+    bloc_id = "default"
+    {:ok, rev, content} = get_bloc(bloc_id)
+    render conn, "index.html", id: bloc_id, rev: rev, content: content
   end
 end
