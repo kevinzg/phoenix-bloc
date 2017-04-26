8c47b9de19b4819dbca3257c3e6f1400cc371387
Kevin Zúñiga <kevin.zun@gmail.com>
2017-04-26 17:49:56 -0500

    Remove quotation marks from bloc default values

--- lib/phoenix_bloc/bloc/bloc_controller.ex
+++ lib/phoenix_bloc/bloc/bloc_controller.ex
@@ -3,7 +3,7 @@ defmodule PhoenixBloc.Bloc.BlocController do
 
   def get_bloc(bloc_id) do
     case Redix.command(:redix, ~w(HMGET bloc:#{bloc_id} rev content)) do
-      {:ok, [:nil, _]} -> {:ok, "0", ""}
+      {:ok, [:nil, _]} -> {:ok, 0, nil}
       {:ok, [rev, content]} -> {:ok, rev, content}
     end
   end
