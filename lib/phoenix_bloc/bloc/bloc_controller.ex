f40aee74557c128d532afb87c1b6f2d46f37c659
Kevin Zúñiga <kevin.zun@gmail.com>
2017-04-27 11:36:35 -0500

    Get new redis structure

--- lib/phoenix_bloc/bloc/bloc_controller.ex
+++ lib/phoenix_bloc/bloc/bloc_controller.ex
@@ -2,7 +2,7 @@ defmodule PhoenixBloc.Bloc.BlocController do
   alias Redix
 
   def get_bloc(bloc_id) do
-    case Redix.command(:redix, ~w(HMGET bloc:#{bloc_id} rev content)) do
+    case Redix.command(:redix, ~w(HMGET bloc:#{bloc_id}:content rev content)) do
       {:ok, [:nil, _]} -> {:ok, 0, nil}
       {:ok, [rev, content]} -> {:ok, rev, content}
     end
